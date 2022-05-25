d49f01f79ce7c4c1c724b03f81b86c677982748d
ridwan <ridwanediallo99@gmail.com>
2022-05-25 15:43:01 +0000

    Add header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Detail from './components/Detail';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route  path="/detail" element={<Detail />} />
