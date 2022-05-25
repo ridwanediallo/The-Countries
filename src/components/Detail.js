803f0dbb925d1d9d4b1ab5022ff98232df417c3c
ridwan <ridwanediallo99@gmail.com>
2022-05-25 14:31:27 +0000

    Add more detail in detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Modal, Button } from 'react-bootstrap';
 
 const Detail = () => {
   const countries = useSelector((state) => state.allCountries);
@@ -7,24 +8,32 @@ const Detail = () => {
   const filteredCountry = countries.filter(
     (country) => country.clicked === true
   );
-   console.log(filteredCountry)
+  console.log(filteredCountry);
   return (
     <div className="card mb-3">
       {filteredCountry.map((country) => {
         return (
           <div key={country.name}>
-            <img src={country.flag} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{country.name}</h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
+            <Modal.Dialog>
+              <Modal.Header>
+                <Modal.Title>{country.name}</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body>
+                <img
+                  src={country.flag}
+                  className="card-img-top pb-4"
+                  alt="..."
+                />
+                <h5 className="py-2">{`Population: ${country.population}`}</h5>
+                <h5 className="py-2">{`Region: ${country.region}`}</h5>
+                <h5 className="py-2">{`Continent: ${country.continent}`}</h5>
+                <h5 className="py-2">{`Subregion: ${country.subregion}`}</h5>
+                <h5 className="py-2">{`Timezone: ${country.timezone}`}</h5>
+              </Modal.Body>
+
+              <Modal.Footer>{/* <h5>{country.population}</h5> */}</Modal.Footer>
+            </Modal.Dialog>
           </div>
         );
       })}
