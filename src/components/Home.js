2d9ed992a5737db7ca484b12e275fdb690e17e08
ridwan <ridwanediallo99@gmail.com>
2022-05-25 22:01:32 +0000

    Add search component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { countryDetails, getCountriesData } from '../redux/CountriesSlice';
 import './Home.css';
+import Search from './Search';
 
 const Home = () => {
+  const [query, setQuery] = useState('');
+
   const dispatch = useDispatch();
   const countries = useSelector((state) => state.allCountries);
-  console.log(countries);
 
   useEffect(() => {
     dispatch(getCountriesData());
@@ -18,38 +20,32 @@ const Home = () => {
     dispatch(countryDetails(id));
   };
 
-  return (
-    <div>
-      <div className="wrap">
-        {countries.map((country) => (
-          <div key={country.name} className="country-card">
-            <Link to='/detail'>
-              <img
-                id={country.name}
-                src={country.flag}
-                alt={`${country.name}'s flag`}
-                onClick={countryDetailsHandler}
-              />
-            </Link>
-            <p>{country.name}</p>
-            <p>{country.population}</p>
+  const queryHandler = (e) => {
+    setQuery(e.target.value);
+  };
 
-            {/* <div className="card">
-                    <img
-                      src={country.flag}
-                      className="card-img-top"
-                      alt={`${country.name}'s flag`}
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">{country.name}</h5>
-                      <p className="card-text">{`Population: ${country.population}`}</p>
-                    </div>
-                  </div>
-                */}
-          </div>
-        ))}
+  return (
+    <>
+      <Search query={query} queryChangeHandler={queryHandler} />
+      <div>
+        <div className="wrap">
+          {countries.map((country) => (
+            <div key={country.name} className="country-card">
+              <Link to="/detail">
+                <img
+                  id={country.name}
+                  src={country.flag}
+                  alt={`${country.name}'s flag`}
+                  onClick={countryDetailsHandler}
+                />
+              </Link>
+              <p>{country.name}</p>
+              <p>{country.population}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/Search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const Search = ({ query, queryChangeHandler }) => {
+  return (
+    <Form className='container'>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control
+          type="email"
+          value={query}
+          placeholder="Search a country"
+          onChange={queryChangeHandler}
+        />
+      </Form.Group>
+    </Form>
+  );
+};
+
+export default Search;
