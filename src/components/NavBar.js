60305c62b9d79955cb1c8b46f8959959505d943f
ridwan <ridwanediallo99@gmail.com>
2022-05-26 10:26:05 +0000

    Add icons to navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { FaAngleLeft } from 'react-icons/fa';
+import { FaMicrophone } from 'react-icons/fa';
+
 
 import './NavBar.css'
 
@@ -7,12 +11,15 @@ const Header = () => {
   return (
     <nav>
       <Link to="/" className="link-home">
-        <h2 className="home">Home</h2>
+        <div className="home">
+          <FaAngleLeft size="2em" />
+          <h2>Home</h2>
+        </div>
       </Link>
       <p className="middle-text">Countries Info</p>
       <div className="icons">
-        <span>icon 1</span>
-        <span>icon 2</span>
+        <FaMicrophone size="1.5em" />
+        <FiSettings size="1.5em" />
       </div>
     </nav>
   );
