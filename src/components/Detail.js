16244ecccb7d63a493cbb57d7b786ba366d8345f
ridwan <ridwanediallo99@gmail.com>
2022-05-26 20:22:17 +0000

    Change home and detail page style and layout

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useSelector} from 'react-redux';
-import { Modal} from 'react-bootstrap';
-import './Detail.css'
+import { useSelector } from 'react-redux';
+import { Modal } from 'react-bootstrap';
+import './Detail.css';
 
 const Detail = () => {
   const countries = useSelector((state) => state.allCountries);
@@ -10,35 +10,28 @@ const Detail = () => {
     (country) => country.clicked === true
   );
   return (
-    <div className="card mb-3">
-      {filteredCountry.map((country) => {
-        return (
-          <div key={country.name}>
-            <Modal.Dialog>
-              <Modal.Header>
-                <Modal.Title>{country.name}</Modal.Title>
-              </Modal.Header>
-
-              <Modal.Body>
-                <img
-                  src={country.flag}
-                  className="card-img-top pb-4"
-                  alt={`${country.region}'s flag`}
-                />
-                <h5 className="py-2">{`Population: ${country.population}`}</h5>
-                <h5 className="py-2">{`Region: ${country.region}`}</h5>
-                <h5 className="py-2">{`Continent: ${country.continent}`}</h5>
-                <h5 className="py-2">{`Subregion: ${country.subregion}`}</h5>
-              </Modal.Body>
-
-              <Modal.Footer>
-                <h5 className="py-2">{`Timezone: ${country.timezone}`}</h5>
-              </Modal.Footer>
-            </Modal.Dialog>
-          </div>
-        );
-      })}
-    </div>
+    <section>
+      {filteredCountry.map((country) => (
+        <ul key={country.name} className="items">
+          <li className="item p-2">
+            <p className="population">Population: </p>
+            <p className="population">{country.population}</p>
+          </li>
+          <li className="item p-2">
+            <p className="population">continent: </p>
+            <p className="population">{country.continent}</p>
+          </li>
+          <li className="item p-2">
+            <p className="population">Subregion: </p>
+            <p className="population">{country.subregion}</p>
+          </li>
+          <li className="item p-2">
+            <p className="population">Timezone: </p>
+            <p className="population">{country.timezone}</p>
+          </li>
+        </ul>
+      ))}
+    </section>
   );
 };
 
